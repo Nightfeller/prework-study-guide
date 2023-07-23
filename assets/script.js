96f683a0238a822c42de1870da9783002417c6c7
Nightfeller <family2earls@gmail.com>
2023-07-23 14:40:21 -0500

    added basic JavaScript to project

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+//Updated variable with an array.
+for(var i =0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
+
+/*
+if(topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if(topic === 'CSS') {
+    console.log("Let's study CSS");
+} else if(topic === 'Git') {
+    console.log ("Let's study Git!");
+} else if(topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+//Checks what topic is currently being studied.
+} else {
+    console.log('Please try again!');
+//Failsafe/bug detector in case something goes wrong.
+}
+*/
